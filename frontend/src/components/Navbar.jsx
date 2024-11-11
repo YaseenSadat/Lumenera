21fde23aa274cb495ab3411e6ce7db0bc70b85bd
YaseenSadat <yaseensadat2@gmail.com>
2024-11-11 16:01:56 -0500

    edits

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {assests} from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div className='flex item-center justify-between py-5 font medium'>
+    <div className='flex items-center justify-between py-5 font-medium'>
         
         <img src={assests.logo} className='w-36' alt="" />
 
