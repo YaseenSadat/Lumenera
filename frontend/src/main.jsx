91e0da19473501cbf2dfce4a49027dba2889a4b3
YaseenSadat <yaseensadat2@gmail.com>
2024-11-12 17:45:32 -0500

    new changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
+import ShopContextProvider from './context/ShopContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <ShopContextProvider>
     <App />
+  </ShopContextProvider>
   </BrowserRouter>
 )
+
+{/* 1:10:49 LAST TOUCH */}
